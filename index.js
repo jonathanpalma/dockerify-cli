514570f01b4a31247fc352425c2939e2d9fb71fd
Jonathan Palma <tanpalma04@gmail.com>
2019-03-10 01:22:43 -0600

    Base project

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const pkg = require('./package');
+const { build, init } = require('./lib');
+
+program.version(pkg.version);
+
+program.command('build')
+  .alias('b')
+  .description('Build image from Dockerfile')
+  .action(build);
+
+program.command('init')
+  .alias('i')
+  .description('Geneate Dockerfile, .dockerignore and server conf')
+  .action(init);
+
+
+program.parse(process.argv);
